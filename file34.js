242b1afd6a3617f7ae6771c901ec078613445d2f
vishnurejii <vishnurejii2003@gmail.com>
2026-02-16 10:50:39 +0530

    new commit

---
+++ file34.js
@@ -0,0 +1,24 @@
+// function response(user){
+//     console.log(user.name)
+//     console.log(user.email)
+// }
+
+// const user={
+//     name:"john",
+//     email:"john@gmail.com",
+//     role:"user"
+// }
+// response(user)
+
+
+function response({name,email}){
+    console.log(name)
+    console.log(email)
+}
+
+const user={
+    name:"john",
+    email:"john@gmail.com",
+    role:"user"
+}
+response(user)
